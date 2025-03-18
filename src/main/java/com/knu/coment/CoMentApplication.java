f46d8c6154b91478270f1601645342fa0752551f
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-03-18 15:30:33 +0900

    Initial commit

---
+++ src/main/java/com/knu/coment/CoMentApplication.java
@@ -0,0 +1,13 @@
+package com.knu.coment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoMentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoMentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/knu/coment/CoMentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knu.coment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoMentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
