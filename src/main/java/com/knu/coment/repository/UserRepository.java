749712db112e8305e99038bbe16fd8ab32d6020a
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-03-25 16:26:39 +0900

    :bug: lazy 버그 수정

--- src/main/java/com/knu/coment/repository/UserRepository.java
+++ src/main/java/com/knu/coment/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.knu.coment.repository;
 
 import com.knu.coment.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = "userStacks")
     Optional<User> findByGithubId(String githubId);
-    Optional<User> findByEmail(String email);
 }
