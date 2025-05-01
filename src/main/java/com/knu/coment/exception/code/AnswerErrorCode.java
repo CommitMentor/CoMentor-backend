26e7171451fd2079b80039a3a4655c20a17db8c8
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-05-02 00:06:11 +0900

    CS 연습하기 프롬프트(마크업, 설명 코드 추가) 수정, 문제 다시 풀기 API 추가

--- src/main/java/com/knu/coment/exception/code/AnswerErrorCode.java
+++ src/main/java/com/knu/coment/exception/code/AnswerErrorCode.java
@@ -11,7 +11,9 @@ public enum AnswerErrorCode implements ErrorCode {
     NOT_FOUND_ANSWER(HttpStatus.NOT_FOUND, "답변을 찾을 수 없습니다."),
     ALREADY_DONE_ANSWER(HttpStatus.BAD_REQUEST, "이미 답변이 완료된 질문입니다."),
     UNAUTHORIZED_QUESTION_ACCESS(HttpStatus.FORBIDDEN, "해당 질문에 대한 권한이 없습니다."),
-    NOT_RECOMMENDED_QUESTION(HttpStatus.BAD_REQUEST, "추천된 질문이 아닙니다.");
+    NOT_RECOMMENDED_QUESTION(HttpStatus.BAD_REQUEST, "추천된 질문이 아닙니다."),
+    UPDATE_ERROR(HttpStatus.BAD_REQUEST, "답변을 입력해주세요"),
+    FEEDBACK_PARSE_ERROR(HttpStatus.NOT_FOUND, "피드백 변환 오류"),;
 
 
     private final HttpStatus httpStatus;
